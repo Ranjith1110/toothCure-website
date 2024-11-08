11c748d02dfe344590d41e2e9c0a30a43d123420
Ranjith <ranjithram878@gmail.com>
2024-11-08 18:03:53 +0530

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -107,14 +107,28 @@
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
         smartSpeed: 1000,
-        items: 1,
+        center: true,
         dots: false,
         loop: true,
         nav : true,
         navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
         ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            },
+            992:{
+                items:3
+            }
+        }
     });
     
 })(jQuery);
